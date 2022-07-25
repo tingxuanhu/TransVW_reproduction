b767ecab16668802d780532775516000950c36f9
tingxuanhu <105578021+tingxuanhu@users.noreply.github.com>
2022-07-25 23:39:24 +0800

    Update original_dicom_preprocessing.py

--- utils/original_dicom_preprocessing.py
+++ utils/original_dicom_preprocessing.py
@@ -3,7 +3,7 @@ import shutil
 
 path = '/home/data/tingxuan/Research'
 
-path_to_save = '/home/data/tingxuan/DatasetForTest'
+path_to_save = '/home/data/tingxuan/DICOM'
 
 if not os.path.exists(path_to_save):
     os.makedirs(path_to_save)
@@ -18,37 +18,39 @@ for i in directory:
         # print(num_person)
 
         for num in num_person:
-            if num == '1':
-                image_file = os.path.join(people, num)
+            # if num == '1':
+            #     image_file = os.path.join(people, num)
                 # print(image_file)
 
-                for img in os.listdir(image_file):
-                    if img != '.DS_Store' and img != 'VERSION' and img != 'LOCKFILE' and img != 'DICOMDIR':
-                        image_files = os.path.join(image_file, img)
-                        # print(image_files)
+            image_file = os.path.join(people, num)
 
-                        for img_ in os.listdir(image_files):
-                            if img_ != '.DS_Store' and img_ != 'VERSION':
-                                # print(img_)
+            for img in os.listdir(image_file):
+                if img != '.DS_Store' and img != 'VERSION' and img != 'LOCKFILE' and img != 'DICOMDIR':
+                    image_files = os.path.join(image_file, img)
+                    # print(image_files)
 
-                                image_end = os.path.join(image_files, img_)
-                                # print(image_end)
+                    for img_ in os.listdir(image_files):
+                        if img_ != '.DS_Store' and img_ != 'VERSION':
+                            # print(img_)
 
-                                path_to_save_images = os.path.join(path_to_save, img_)
-                                # print(path_to_save_images)
+                            image_end = os.path.join(image_files, img_)
+                            # print(image_end)
 
-                                for figure in os.listdir(image_end):
-                                    if figure != 'VERSION' and figure != '.DS_Store':
-                                        # print(figure)
-                                        target_figure = os.path.join(image_end, figure)
+                            path_to_save_images = os.path.join(path_to_save, img_)
+                            # print(path_to_save_images)
 
-                                        if not os.path.exists(path_to_save_images):
-                                            os.makedirs(path_to_save_images)
+                            for figure in os.listdir(image_end):
+                                if figure != 'VERSION' and figure != '.DS_Store':
+                                    # print(figure)
+                                    target_figure = os.path.join(image_end, figure)
 
-                                        judge_if_exist = os.path.join(path_to_save_images, figure)
-                                        # print(judge_if_exist)
+                                    if not os.path.exists(path_to_save_images):
+                                        os.makedirs(path_to_save_images)
 
-                                        if not os.path.exists(judge_if_exist):
-                                            shutil.copy(target_figure, path_to_save_images)
+                                    judge_if_exist = os.path.join(path_to_save_images, figure)
+                                    # print(judge_if_exist)
 
-                                print(img_ + 'is finished!')
+                                    if not os.path.exists(judge_if_exist):
+                                        shutil.copy(target_figure, path_to_save_images)
+
+                            print(img_ + ' is finished!')
