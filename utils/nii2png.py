aaa6c7435ebaebd22fb059a057500606b059c68e
tingxuanhu <105578021+tingxuanhu@users.noreply.github.com>
2022-07-27 09:55:19 +0800

    Update nii2png.py

--- utils/nii2png.py
+++ utils/nii2png.py
@@ -14,29 +14,30 @@ def list_file(directory):
 
 
 def nii2img(nii_path, nii_list, img_path_to_save):
-
-    for nii in nii_list:
-        nii_path_to_load = os.path.join(nii_path, nii)
-        nii_img = nib.load(nii_path_to_load)
-        nii_fdata = nii_img.get_fdata()
-        filename = nii.replace('.nii', '')  # delete the postfix name
-
+    for idx, nii in enumerate(nii_list):
+        filename = nii.split("/")[-1].split(".")[0]
         path_to_save = os.path.join(img_path_to_save, filename)
+        print(path_to_save)
 
         if not os.path.exists(path_to_save):
             os.makedirs(path_to_save)
+
+        nii_path_to_load = os.path.join(nii_path, nii)
+        nii_img = nib.load(nii_path_to_load)
+        nii_fdata = nii_img.get_fdata().astype(np.uint8)
 
         # change nii file to png file
         (x, y, z) = nii_img.shape
 
         for i in range(z):
             slice = nii_fdata[:, :, i]
-            imageio.imwrite(os.path.join(path_to_save, f'{i}.png'), slice)
+            imageio.imwrite(os.path.join(path_to_save, f'patient{idx}_slice{i}.png'), slice)
 
 
 nii_path = "/home/data/tingxuan/NII"
 nii_list = list_file(nii_path)
 print(nii_list)
 
-img_path_to_save = '/home/data/tingxuan/PNGImage'
+img_path_to_save = '/home/data/tingxuan/PNG'
 nii2img(nii_path, nii_list, img_path_to_save)
+
